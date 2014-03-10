59315143f65c97cc87942b1ec4c791beeffdd093
ctejada <Ctejada91@gmail.com>
2014-03-09 19:49:49 -0500

    Update GedcomApp.java
    Changed some of the code around.

--- src/GedcomApp.java
+++ src/GedcomApp.java
@@ -3,36 +3,23 @@ import java.io.File;
 
 public class GedcomApp 
 {
-	public static void main(String args[]) 
-	{
-		if(args.length != 1)
-		{
-			System.out.println("Enter a GEDCOM filename argument");
-		}
-		else
-		{
-			String fileName = args[0];
-			File file = new File(fileName);
-			if(!file.exists())
-			{
-				System.out.println("File '"+fileName+"' can't be found or doesn't exist");
-			}
-			else
-			{
-				GedcomReader parser = new GedcomReader();
-				parser.readGED(file);
-				ErrorList pl = parser.findErrors();
-				
-				if ( pl.size() == 0 ) 
-				{
-					System.out.println("File parsed! No Errors Found.");
-				}
-				else
-				{
-					System.out.println( pl.buildOutputString() );
-				}
-			}
-		}
-	}
-	
+    public static void main(String args[]) 
+    {
+        String fileName = "GedcomFile.ged";
+        File file = new File(fileName);
+        if(!file.exists())
+            System.out.println("File '"+fileName+"' can't be found.");
+        else
+        {
+            GedcomReader parser = new GedcomReader();
+            parser.readGED(file);
+            ErrorList pl = parser.findErrors();
+            
+            if ( pl.size() == 0 )
+                System.out.println("No Errors Found.");
+            else
+                System.out.println( pl.buildOutputString() );
+        }
+    }
+    
 }
